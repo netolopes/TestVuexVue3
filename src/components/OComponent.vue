5a1a2d9d65346e29f240ea87e02b6a6cc34e6674
netolopes <netolopes27@gmail.com>
2021-03-23 20:48:08 -0300

    inicial

---
+++ src/components/OComponent.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+
+
+<div class="card card-body m-4">
+    <h3>Forma simples pegar estado (sem mapstate): {{ $store.state.counter }}</h3>
+    <h3>Forma simples pegar resultado do metodo (sem mapMutations): </h3>
+    <button @click=" $store.commit('increaseConter')">enviar(mutations)</button>
+    <h3>Forma simples pegar resultado da action (sem mapActions): </h3>
+    <button @click=" $store.dispatch('increaseConter_2')">enviar(actions)</button>
+     <h3>Forma simples pegar resultado getters (sem mapGetters): </h3>
+    {{ $store.getters.exibeStateSemMutations  }}
+    
+<!--input type="text" v-model="$store.state.colorCode" placeholder="Enter color code" >
+<div :style="{color: $store.state.colorCode}">texto teste</div -->
+<input type="text" v-model="colorCode" placeholder="Enter color code" >
+<div :style="{color: $store.state.colorCode}">texto teste</div>
+
+    <h3> ############## Usando (mapGetters, mapMutations, mapState ) - Options API + Vuex ############## </h3>
+    <h5 class="text-center">{{ counter }} x 2 = {{ times2 }}</h5>
+    <button class="btn btn-primary" @click="inc">Increment</button>
+  </div>
+
+
+  </div>
+
+</template>
+<script>
+import { mapGetters, mapMutations, mapState } from 'vuex'
+
+export default {
+  computed: {
+    ...mapState(['counter']),
+    ...mapGetters(['times2']),
+
+    colorCode:{
+      get(){
+        return this.$store.state.colorCode
+      },
+      set(newValue){
+        //this.$store.commit('setColorCode',newvalue)
+         this.$store.dispatch('setColorCode',newValue)
+      }
+    }
+  },
+  methods: {
+    ...mapMutations(['setCounter']),
+    inc() {
+      this.setCounter(this.counter + 1)
+    }
+  }
+}
+</script>
+
+
--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,49 @@
 import { createStore } from 'vuex'
+import axios from 'axios'
+
 
 export default createStore({
   state: {
+    counter: 150,
+    colorCode: 'green'
+  },
+  getters: {
+    times2(state) {
+      return state.counter * 2
+    },
+    exibeStateSemMutations(state){
+      return state.counter * 999999
+    }
   },
   mutations: {
+    setCounter(state, value) {
+      state.counter = value
+    },
+    setColorCode(state, newvalue){
+      state.colorCode = newvalue
+    },
+    increaseConter(state){
+      state.counter--
+    },
+    increaseConter_2(state, numRandom){
+      console.log('numRandom' , numRandom);
+      state.counter--
+    }
   },
   actions: {
-  },
-  modules: {
+    //faz uma requisicao post antes de executar o metodo e deppois passa
+    // cm parametro para a funcao increaseConter_2
+     // console.log('entrou na action')
+      increaseConter_2({ commit }) 
+      {
+      axios('https://www.random.org/integers/?num=1&min=1&max=6&col=1&base=10&format=plain&rnd=new').then(response => {
+       // console.log('response', response)
+       commit('increaseConter_2', response.data) //response.data == numRandom
+      })
+    },
+    setColorCode({ commit }, newvalue )
+    {
+      commit('setColorCode', newvalue)
+    }
   }
 })
